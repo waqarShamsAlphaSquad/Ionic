60a09dfd4dbcfe2d4f6e5bbccd5e31e8886637bf
waqarShamsAlphaSquad <waqar.alphasquad@gmail.com>
2023-08-11 09:24:51 -0700

    First Push

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules:["@nuxtjs/ionic"],
+  ionic: {
+    integrations: {
+      //
+    },
+    css: {
+      //
+    },
+    config: {
+      //
+    }
+  },
+})
---
+++ pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+    fsadfsf
+</template>
